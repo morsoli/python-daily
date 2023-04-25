0846b58633b77c1c556712c5ba0f869630428657
morso <morso@morsodeMacBook-Pro.local>
2023-04-25 13:04:46 +0800

    podcast工具

---
+++ podcast-note/main.py
@@ -0,0 +1,81 @@
+ PODCAST_INFO = [
+    {
+        "name": "Lex Fridman",
+        "description": "麻省理工学院的人工智能专家Lex Fridman和知名科学家、企业家、作家等嘉宾进行深入对话,探讨人工智能、机器人、自动驾驶、生物技术等热门话题。",
+        "metadata": {
+            "podcast_url": "",
+            "official_website": "",
+            "youtube_url": "https://www.youtube.com/@lexfridman",
+        }
+    },{
+        "name": "Huberman Lab",
+        "description": "斯坦福大学的神经科学家Andrew Huberman帮助听众了解大脑如何影响身心健康、睡眠、情绪、创造力等。",
+        "metadata": {
+            "podcast_url": "",
+            "official_website": "",
+            "youtube_url": "https://www.youtube.com/@hubermanlab",
+        }
+    },{
+        "name": "WorkLife",
+        "description": "Adam Grant曾是沃顿商学院最年轻的教授,并已经连续7年成为沃顿商学院的最佳教授。也是《纽约时报》排名第一的畅销书作家。他的播客讲述了我们如何在工作中找到动力、创意和乐趣。",
+        "metadata": {
+            "podcast_url": "",
+            "official_website": "https://adamgrant.net/podcasts/work-life/",
+            "youtube_url": "",
+        }
+    },{
+        "name": "Dare to Lead",
+        "description": "Brené Brown是休斯顿大学的社会工作教授, 研究勇气、脆弱性和同理心。她曾在TED演讲中分享过自己对脆弱性的研究和体验,引起了全球观众的共鸣。",
+        "metadata": {
+            "podcast_url": "",
+            "official_website": "https://brenebrown.com/podcast-show/dare-to-lead/",
+            "youtube_url": "",
+        }
+    },{
+        "name": "Unlocking Us",
+        "description": "Brené Brown是休斯顿大学的社会工作教授, 研究勇气、脆弱性和同理心。她曾在TED演讲中分享过自己对脆弱性的研究和体验,引起了全球观众的共鸣。",
+        "metadata": {
+            "podcast_url": "",
+            "official_website": "https://brenebrown.com/podcast-show/unlocking-us/",
+            "youtube_url": "",
+        }
+    }          
+    ]
+
+
+def download_audio(url):
+    pass
+
+def download_video_txt(url):
+    pass
+
+def download_video_srt(url):
+    pass
+
+def download_transcript(url):
+    pass
+
+def trans_audio_to_text(audio):
+    pass
+
+def trans_txt_to_text(txt):
+    pass
+
+def trans_srt_to_text(srt):
+    pass
+
+def trans_english_to_chinese(text):
+    pass
+
+def trans_text_to_pdf(text):
+    pass
+
+def trans_text_to_epub(text):
+    pass
+
+def gernerate_summary(text ):
+    pass
+
+
+if __name__ == '__main__':
+    pass
